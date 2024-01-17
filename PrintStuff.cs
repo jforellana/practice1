820301e9204a09077730f77987142120ef028915
jforellana <mrjfob@byu.edu>
2024-01-16 22:22:33 -0700

    Add project files.

---
+++ PrintStuff.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.SymbolStore;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practice1
+{
+    internal class PrintStuff
+    {
+        private string language;
+
+        public PrintStuff()
+        {
+            language = "EN";
+        }
+        public PrintStuff(string temp)
+        {
+            language = temp;
+        }
+        public void PrintName(string n)
+        {
+
+            if (language == "EN")
+            {
+                System.Console.WriteLine("Hello " + n);
+            }
+            
+            if (language == "SPA")
+            {
+                System.Console.WriteLine("Hola " + n);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using practice1;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        PrintStuff ps;
+
+        ps = new PrintStuff("SPA");
+
+        System.Console.WriteLine("Please enter your name ");
+        string name = "";
+        name = System.Console.ReadLine();
+
+        ps.PrintName(name);
+    }
+}
